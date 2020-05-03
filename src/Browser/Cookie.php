81057abf678e01c9d91c713321f653037c8520ca
mohmmad.m.othman <mohmmad.m.othman@gmail.com>
2020-05-03 14:56:23 +0200

    Adding Php Doc

--- src/Browser/Cookie.php
+++ src/Browser/Cookie.php
@@ -2,8 +2,12 @@
 
 namespace BrowserBotPHP\Browser\Browser;
 
-use JsonSerializable;
 
+
+/**
+ * Cookie for Browser
+ * Class Cookie
+ */
 class Cookie
 {
 
@@ -21,11 +25,8 @@ class Cookie
     public $id = null;
     function __construct()
     {
-        $get_arguments       = func_get_args();
-        $number_of_arguments = func_num_args();
-
-        if (method_exists($this, $method_name = '__construct' . $number_of_arguments)) {
-            call_user_func_array(array($this, $method_name), $get_arguments);
+        if (method_exists($this, $method_name = '__construct' . func_num_args())) {
+            call_user_func_array(array($this, $method_name),  func_get_args());
         }
     }
     function __construct10(
